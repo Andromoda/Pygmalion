91d97d7af6441c445089a2c8848b170a285686d2
Tahgolov <supernovavk@hotmail.com>
2024-11-30 20:50:22 +0300

    optimize native hooks

--- library/src/main/cpp/hooks/hook_register_natives.cpp
+++ library/src/main/cpp/hooks/hook_register_natives.cpp
@@ -2,24 +2,19 @@
 // Created by vologhat on 10/20/24.
 //
 
-#include <string>
+#include <jni_aux.h>
 #include <dlfcn.h>
+
+#include "macros.h"
 
 #include "hooks/hook_register_natives.h"
 #include "hooks/assetman/hook_assetman.h"
 
-#include "macros.h"
-
-static inline bool operator==(const JNINativeMethod& a,const JNINativeMethod& b)
+/** return the count of replaced JNI functions */
+int8_t replace_ptrs(JNINativeMethod* methods,int nmethods,
+                    jni_hook_t gMethodHooks[],int nhooks)
 {
-    return !strcmp(a.name,b.name)
-           &&!strcmp(a.signature,b.signature);//for possible overloading
-}
-
-bool replace_ptrs(JNINativeMethod* methods,int nmethods,
-                  jni_hook_t gMethodHooks[],int nhooks)
-{
-    int8_t hooked=0;
+    int8_t replaced=0;
 
     //replace original functions with hooks
     for(auto i=0;i<nmethods;++i)
@@ -27,21 +22,24 @@ bool replace_ptrs(JNINativeMethod* methods,int nmethods,
         auto& target_mtd=methods[i];
         for(auto j=0;j<nhooks;++j)
         {
-            const auto& jni_hook=gMethodHooks[j];
-            if(jni_hook.fn_info==target_mtd)
-            {
-                LOGD("binding hook for function %s%s",target_mtd.name,target_mtd.signature);
+            auto& jni_hook=gMethodHooks[j];
+            if(jni_hook.fn_info!=target_mtd)continue;
+            LOGD("binding hook for function %s%s",target_mtd.name,target_mtd.signature);
 
-                //backup original function address
-                *static_cast< void** >(jni_hook.fn_info.fnPtr)=target_mtd.fnPtr;
-                target_mtd.fnPtr=jni_hook.fn_hook_ptr;
+            //reserve original function address to call it in hooks
+            *static_cast< void** >(jni_hook.fn_info.fnPtr)=target_mtd.fnPtr;
 
-                ++hooked;
-            }
+            //reserve original function address to the hook struct for informational purposes
+            jni_hook.fn_info.fnPtr=target_mtd.fnPtr;
+
+            //replace original function address with hook function address
+            target_mtd.fnPtr=jni_hook.fn_hook_ptr;
+
+            ++replaced;
         }
     }
 
-    return hooked==nhooks;
+    return replaced;
 }
 
 install_hook_name(RegisterNativeMethods,jint,
@@ -63,23 +61,23 @@ install_hook_name(RegisterNativeMethods,jint,
 
 void hook_RegisterNatives(JNIEnv* env)
 {
-    //get address of RegisterNatives function
-    const auto addr=reinterpret_cast< void* >(env->functions->RegisterNatives);
+    using RegisterAssetManager_t=int(*)(JNIEnv*);
+
+    const auto register_natives_addr=reinterpret_cast< void* >(env->functions->RegisterNatives);
 
     //bind hook for RegisterNatives
-    DobbyHook(addr,
+    DobbyHook(register_natives_addr,
               reinterpret_cast< dobby_dummy_func_t >(fake_RegisterNativeMethods),
               reinterpret_cast< dobby_dummy_func_t* >(orig_RegisterNativeMethods));
 
     //trigger RegisterNatives for AssetManager to obtain data
-    const auto register_asset_man=reinterpret_cast< int(*)(JNIEnv*) >(DobbySymbolResolver(LIB_ANDROID_RUNTIME_PATH,
+    const auto register_asset_man=reinterpret_cast< RegisterAssetManager_t >(DobbySymbolResolver(LIB_ANDROID_RUNTIME_PATH,
                                                                                           SIGN_REGISTER_ANDROID_CONTENT_ASSETMANAGER));
     LOGD(SIGN_REGISTER_ANDROID_CONTENT_ASSETMANAGER" addr: %p",register_asset_man);
-
     register_asset_man(env);
 
     //unbind hook
-    DobbyDestroy(addr);
+    DobbyDestroy(register_natives_addr);
 
     gIsInitialized=gAssetManagerMethods&&gHookAssetManagerMethods
                    &&gNumAssetManagerMethods;
@@ -88,9 +86,11 @@ void hook_RegisterNatives(JNIEnv* env)
 jint register_natives(JNIEnv* env)
 {
     if(env->RegisterNatives(env->FindClass("com/vologhat/pygmalion/Pygmalion"),
-                                gMethods,NELEM(gMethods)))
+                            gMethods,NELEM(gMethods)))
         return JNI_ERR;
 
+    //we need to call it from JNI_OnLoad,
+    //in other case it's crashed
     hook_RegisterNatives(env);
 
     return JNI_OK;
@@ -101,12 +101,12 @@ jboolean jis_initialized(JNIEnv*,jclass)
 
 jboolean jhook(JNIEnv* env,jclass)
 {
-    if(gIsInitialized)return hook_assetman(env,gHookAssetManagerMethods,gNumAssetManagerMethods);
-    return false;
+    if(!gIsInitialized)return false;
+    return hook_assetman(env,gHookAssetManagerMethods,gNumAssetManagerMethods);
 }
 
 jboolean junhook(JNIEnv* env,jclass)
 {
-    if(gIsInitialized)return hook_assetman(env,gAssetManagerMethods,gNumAssetManagerMethods);
-    return false;
+    if(!gIsInitialized)return false;
+    return hook_assetman(env,gAssetManagerMethods,gNumAssetManagerMethods);
 }
--- library/src/main/cpp/include/hooks/assetman/api/hooks_postoreo.h
+++ library/src/main/cpp/include/hooks/assetman/api/hooks_postoreo.h
@@ -16,90 +16,47 @@ namespace hook_post_oreo
     static jclass getHookClz(JNIEnv* env)
     { return jni_reflection::find_class(env,"com.vologhat.pygmalion.hooks.api.HookPostOreo"); }
 
-    REGISTER_HOOK(NativeGetResourceValue,"nativeGetResourceValue","(JISLandroid/util/TypedValue;Z)I",
-                  jint,
-                  JNIEnv* env,jclass clazz,
-                  jlong ptr,
-                  jint resid,
-                  jshort density,
-                  jobject typed_value,
-                  jboolean resolve_references)
-    {
-        jint cookie=orig_NativeGetResourceValue(env,clazz,ptr,resid,density,typed_value,resolve_references);
+    HOOK(NativeGetResourceValue,"nativeGetResourceValue","(JISLandroid/util/TypedValue;Z)I",
+         jint,
+         jlong ptr,
+         jint resid,
+         jshort density,
+         jobject typed_value,
+         jboolean resolve_references
+     )
+    { HOOK_BODY_INT(NativeGetResourceValue,env,clazz,getHookClz(env),ptr,resid,density,typed_value,resolve_references); }
 
-        jclass clz=hook_post_oreo::getHookClz(env);
-        const auto& fn_info=gNativeGetResourceValue.fn_info;
-        jmethodID mtd=env->GetStaticMethodID(clz,fn_info.name,fn_info.signature);
-        env->CallStaticIntMethod(clz,mtd,ptr,resid,density,typed_value,resolve_references);
+    HOOK(NativeThemeGetAttributeValue,"nativeThemeGetAttributeValue","(JJILandroid/util/TypedValue;Z)I",
+         jint,
+         jlong ptr,jlong theme_ptr,
+         jint resid,
+         jobject typed_value,
+         jboolean resolve_references
+    )
+    { HOOK_BODY_INT(NativeThemeGetAttributeValue,env,clazz,getHookClz(env),ptr,theme_ptr,resid,typed_value,resolve_references); }
 
-        return cookie;
-    }
+    HOOK(NativeApplyStyle,"nativeApplyStyle","(JJIIJ[IJJ)V",
+         void,
+         jlong ptr,jlong theme_ptr,
+         jint def_style_attr,jint def_style_resid,jlong xml_parser_ptr,
+         jintArray java_attrs,jlong out_values_ptr,jlong out_indices_ptr
+    )
+    { HOOK_BODY_VOID(NativeApplyStyle,env,clazz,getHookClz(env),ptr,theme_ptr,def_style_attr,def_style_resid,xml_parser_ptr,java_attrs,out_values_ptr,out_indices_ptr); }
 
-    REGISTER_HOOK(NativeThemeGetAttributeValue,"nativeThemeGetAttributeValue","(JJILandroid/util/TypedValue;Z)I",
-                  jint,
-                  JNIEnv* env,jclass clazz,
-                  jlong ptr,jlong theme_ptr,
-                  jint resid,
-                  jobject typed_value,
-                  jboolean resolve_references)
-    {
-        jint cookie=orig_NativeThemeGetAttributeValue(env,clazz,ptr,theme_ptr,resid,typed_value,resolve_references);
+    HOOK(NativeResolveAttrs,"nativeResolveAttrs","(JJII[I[I[I[I)Z",
+         jboolean,
+         jlong ptr,jlong theme_ptr,
+         jint def_style_attr,jint def_style_resid,
+         jintArray java_values,jintArray java_attrs,jintArray out_java_values,jintArray out_java_indices
+    )
+    { HOOK_BODY_BOOL(NativeResolveAttrs,env,clazz,getHookClz(env),ptr,theme_ptr,def_style_attr,def_style_resid,java_values,java_attrs,out_java_values,out_java_indices); }
 
-        jclass clz=hook_post_oreo::getHookClz(env);
-        const auto& fn_info=gNativeThemeGetAttributeValue.fn_info;
-        jmethodID mtd=env->GetStaticMethodID(clz,fn_info.name,fn_info.signature);
-        env->CallStaticIntMethod(clz,mtd,ptr,theme_ptr,resid,typed_value,resolve_references);
-
-        return cookie;
-    }
-
-    REGISTER_HOOK(NativeApplyStyle,"nativeApplyStyle","(JJIIJ[IJJ)V",
-                  void,
-                  JNIEnv* env,jclass clazz,
-                  jlong ptr,jlong theme_ptr,
-                  jint def_style_attr,jint def_style_resid,jlong xml_parser_ptr,
-                  jintArray java_attrs,jlong out_values_ptr,jlong out_indices_ptr)
-    {
-        orig_NativeApplyStyle(env,clazz,ptr,theme_ptr,def_style_attr,def_style_resid,xml_parser_ptr,java_attrs,out_values_ptr,out_indices_ptr);
-
-        jclass clz=hook_post_oreo::getHookClz(env);
-        const auto& fn_info=gNativeApplyStyle.fn_info;
-        jmethodID mtd=env->GetStaticMethodID(clz,fn_info.name,fn_info.signature);
-        env->CallStaticVoidMethod(clz,mtd,ptr,theme_ptr,def_style_attr,def_style_resid,xml_parser_ptr,java_attrs,out_values_ptr,out_indices_ptr);
-    }
-
-    REGISTER_HOOK(NativeResolveAttrs,"nativeResolveAttrs","(JJII[I[I[I[I)Z",
-                  jboolean,
-                  JNIEnv* env,jclass clazz,
-                  jlong ptr,jlong theme_ptr,
-                  jint def_style_attr,jint def_style_resid,
-                  jintArray java_values,jintArray java_attrs,jintArray out_java_values,jintArray out_java_indices)
-    {
-        jboolean res=orig_NativeResolveAttrs(env,clazz,ptr,theme_ptr,def_style_attr,def_style_resid,java_values,java_attrs,out_java_values,out_java_indices);
-
-        jclass clz=hook_post_oreo::getHookClz(env);
-        const auto& fn_info=gNativeResolveAttrs.fn_info;
-        jmethodID mtd=env->GetStaticMethodID(clz,fn_info.name,fn_info.signature);
-        env->CallStaticBooleanMethod(clz,mtd,ptr,theme_ptr,def_style_attr,def_style_resid,java_values,java_attrs,out_java_values,out_java_indices);
-
-        return res;
-    }
-
-    REGISTER_HOOK(NativeRetrieveAttributes,"nativeRetrieveAttributes","(JJ[I[I[I)Z",
-                  jboolean,
-                  JNIEnv* env,jclass clazz,
-                  jlong ptr,jlong xml_parser_ptr,
-                  jintArray java_attrs,jintArray out_java_values,jintArray out_java_indices)
-    {
-        jboolean res=orig_NativeRetrieveAttributes(env,clazz,ptr,xml_parser_ptr,java_attrs,out_java_values,out_java_indices);
-
-        jclass clz=hook_post_oreo::getHookClz(env);
-        const auto& fn_info=gNativeRetrieveAttributes.fn_info;
-        jmethodID mtd=env->GetStaticMethodID(clz,fn_info.name,fn_info.signature);
-        env->CallStaticBooleanMethod(clz,mtd,ptr,xml_parser_ptr,java_attrs,out_java_values,out_java_indices);
-
-        return res;
-    }
+    HOOK(NativeRetrieveAttributes,"nativeRetrieveAttributes","(JJ[I[I[I)Z",
+         jboolean,
+         jlong ptr,jlong xml_parser_ptr,
+         jintArray java_attrs,jintArray out_java_values,jintArray out_java_indices
+    )
+    { HOOK_BODY_BOOL(NativeRetrieveAttributes,env,clazz,getHookClz(env),ptr,xml_parser_ptr,java_attrs,out_java_values,out_java_indices); }
 }
 
 static jni_hook_t gHooksPostOreo[]=
